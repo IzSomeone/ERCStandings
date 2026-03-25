0cb14d7489a0b4180bb6c0f8384db3fdd3350fb7
IzSomeone <luvitali79.3@gmail.com>
2026-03-25 20:52:09 +0100

    Refactor Flask routes and file serving methods

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,18 @@
 from flask import Flask, jsonify, request, send_file, send_from_directory, session # type: ignore
 from flask_cors import CORS # type: ignore
-import os
 from supabase import create_client # type: ignore
 
 app = Flask(__name__)
 
 CORS(app, supports_credentials=True)
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
-@app.get("/index")
-def serve_index():
-    return send_file(os.path.join(PROJECT_ROOT, "index.html"))
-
 @app.get("/")
-def serve_index_2():
-    return send_file(os.path.join(PROJECT_ROOT, "index.html"))
+def serve_index():
+    return send_from_directory(".", "index.html")
 
 @app.get("/stile.css")
 def serve_css():
-    return send_file(os.path.join(PROJECT_ROOT, "stile.css"))
-
+    return send_from_directory(".", "stile.css")
 
 url = "https://jynyslmoqfbcjugskbwe.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5bnlzbG1vcWZiY2p1Z3NrYndlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyOTcxNDEsImV4cCI6MjA4OTg3MzE0MX0.3WHn4vCyHFfgnskTZAWfF6IYqysSJxDjcOcVhHQETm8"
